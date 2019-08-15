7eb57f6c316651df73ada8b95b0e815a4e6ccd58
Joel Perez <joelness03@gmail.com>
2019-08-14 20:39:31 -0600

    tablet and desktop media queries complete

--- src/assets/sample_data.js
+++ src/assets/sample_data.js
@@ -6,7 +6,7 @@ export const sampleData = [
   {
     title: "Miracle Messages Map",
     description:
-      "Miracle Messages Map is a web application built to connect homeless people to their families. I was the back-end enginner on the process and I implemented a RESTful API designed using NodeJS with a PostgreSQL database.",
+      "Miracle Messages Map is a web application built to connect homeless people to their families. I was the back-end enginner on the project and I implemented a RESTful API designed using NodeJS with a PostgreSQL database.",
     image: MMM,
     deployment: "https://mmm-api-docs.netlify.com/",
     repo: "https://github.com/miracle-mm/back-end"
@@ -14,7 +14,7 @@ export const sampleData = [
   {
     title: "Tabless Thursday",
     description:
-      "Tabless Thursday is a web application built to save tabs in order to reduce CPU and RAM usage by allowing users to save and close open tabs in browsers. I was one of two assigned to the front end of this application. We decided to build a single page React application using Router and Redux",
+      "Tabless Thursday is a web application built to save tabs in order to reduce CPU and RAM usage by allowing users to save and close open tabs in browsers. I was one of two assigned to the front end of this application. We decided to build a single page React application using Router and Redux.",
     image: Tabless,
     deployment: "https://tabless-thursday-fe.netlify.com/",
     repo:
