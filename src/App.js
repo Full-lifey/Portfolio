72fa6c0337985354074a26a61965cb75cd4cb538
Joel Perez <joelness03@gmail.com>
2019-08-20 09:01:05 -0600

    fixed the image resizing on the banner

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className='App'>
       <ParallaxProvider>
-        <Banner />
+        <Route path='/' component={Banner} />
       </ParallaxProvider>
       <ProjectList />
     </div>
