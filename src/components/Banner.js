9dc2a84c8a88bec2470cce731b5d49eb72a2acef
Joel Perez <joelness03@gmail.com>
2019-10-11 00:03:34 -0600

    fixed banner img width on large screens

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,24 +6,32 @@ import MyImg from "../assets/img/Me.jpg";
 
 import "./Banner.scss";
 
+const imageStyle = {
+  width: "100%"
+};
+
 const Banner = props => {
   return (
-    <div className='banner-container'>
-      <Parallax className='banner-img-wrapper desktop' x={[-80, 80]}>
-        <img className='banner-header-img' src={BannerImg} alt='Banner Img' />
+    <div className="banner-container">
+      <Parallax
+        className="banner-img-wrapper desktop"
+        x={[-80, 80]}
+        styleInner={imageStyle}
+      >
+        <img className="banner-header-img" src={BannerImg} alt="Banner Img" />
       </Parallax>
-      <Parallax className='banner-story desktop' x={[80, -80]}>
-        <div className='banner-header'>
-          <img className='my-img' src={MyImg} alt='Me' />
+      <Parallax className="banner-story desktop" x={[80, -80]}>
+        <div className="banner-header">
+          <img className="my-img" src={MyImg} alt="Me" />
           <h1>Joel Perez</h1>
         </div>
-        <div className='banner-subheader'>
+        <div className="banner-subheader">
           <p>
             Full-stack software engineer with extensive experience in server and
             networking infrastructure.
           </p>
         </div>
-        <p className='banner-detail'>
+        <p className="banner-detail">
           I've been in technology for 15 years. From 2013 to 2017 I ran the IT
           department at PCS Ferguson. This involved ~25 servers, ~30 networks,
           and ~400 employees. In 2017 I was promoted to run the server
@@ -37,26 +45,26 @@ const Banner = props => {
           writing code I am a competitive golfer, avid outdoorsman, and
           skier/snowboarder.
         </p>
-        <div className='banner-btn'>
-          <a href='mailto:joel@joelperez.dev'>Contact Me</a>
+        <div className="banner-btn">
+          <a href="mailto:joel@joelperez.dev">Contact Me</a>
         </div>
       </Parallax>
-      <div className='mobile'>
-        <div className='banner-img-wrapper'>
-          <img className='banner-header-img' src={BannerImg} alt='Banner Img' />
+      <div className="mobile">
+        <div className="banner-img-wrapper">
+          <img className="banner-header-img" src={BannerImg} alt="Banner Img" />
         </div>
-        <div className='banner-story'>
-          <div className='banner-header'>
+        <div className="banner-story">
+          <div className="banner-header">
             <h1>Joel Perez</h1>
-            <img className='my-img' src={MyImg} alt='Me' />
+            <img className="my-img" src={MyImg} alt="Me" />
           </div>
-          <div className='banner-subheader'>
+          <div className="banner-subheader">
             <p>
               Full-stack software engineer with extensive experience in server
               and networking infrastructure.
             </p>
           </div>
-          <p className='banner-detail'>
+          <p className="banner-detail">
             I've been in technology for 15 years. From 2013 to 2017 I ran the IT
             department at PCS Ferguson. This involved ~25 servers, ~30 networks,
             and ~400 employees. In 2017 I was promoted to run the server
@@ -70,8 +78,8 @@ const Banner = props => {
             When not writing code I am a competitive golfer, avid outdoorsman,
             and skier/snowboarder.
           </p>
-          <div className='banner-btn'>
-            <a href='mailto:joel@joelperez.dev'>Contact Me</a>
+          <div className="banner-btn">
+            <a href="mailto:joel@joelperez.dev">Contact Me</a>
           </div>
         </div>
       </div>
